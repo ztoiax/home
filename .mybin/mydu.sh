f559942e59dc2a01d0997e1eed5e004836a2ab8d
ztoiax <liang1124486160@gmail.com>
2020-08-16 17:32:13 +0800

    rm mybin

--- .mybin/mydu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# == 0 ]; then
-     sudo du -cha --max-depth=1 . | grep -E "M|G" | sort -h
-else
-    for i in $@ ; do
-        sudo du -cha --max-depth=1 $i | grep -E "M|G" | sort -h
-        echo \n
-    done
-fi
--- .mybin/tmp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in "$@";do
-    if [ -f /tmp/$i ];then
-        mv $i /tmp/$i`date`
-    else
-        mv $i /tmp/$i
-    fi
-done
